e1df7c51b37bd73ced42c877264564b109020687
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-11-29 16:19:37 +0100

    Extract location data verification

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -2,24 +2,28 @@ const asyncHandler = require('express-async-handler');
 
 const Location = require('../models/location');
 
+async function verifyLocationData(req, res) {
+  const { x, y } = req.body.clickCoordinates;
+  const { itemName } = req.body;
+
+  // Check for coordinates and matching name
+  const validLocation = await Location.findOne({
+    $and: [
+      { leftBorderCoordinate: { $lte: x } },
+      { rightBorderCoordinate: { $gte: x } },
+      { topBorderCoordinate: { $lte: y } },
+      { bottomBorderCoordinate: { $gte: y } },
+      { name: itemName },
+    ],
+  });
+  console.log(validLocation);
+  return res.send(validLocation ? 'true' : 'false');
+}
+
 exports.verifyLocation = [
   // TODO: sanitize?
 
   asyncHandler(async (req, res) => {
-    const { x, y } = req.body.clickCoordinates;
-    const { itemName } = req.body;
-
-    // Check for coordinates and matching name
-    const validLocation = await Location.findOne({
-      $and: [
-        { leftBorderCoordinate: { $lte: x } },
-        { rightBorderCoordinate: { $gte: x } },
-        { topBorderCoordinate: { $lte: y } },
-        { bottomBorderCoordinate: { $gte: y } },
-        { name: itemName },
-      ],
-    });
-    console.log(validLocation);
-    return res.send(validLocation ? 'true' : 'false');
+    verifyLocationData(req, res);
   }),
 ];

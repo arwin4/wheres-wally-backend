8e09cd6d041128f1dc6d1ace798c06faad1515ab
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-11-30 13:17:21 +0100

    Keep track of user's game progress

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,12 +1,14 @@
 const asyncHandler = require('express-async-handler');
 
+// Models
 const Location = require('../models/location');
+const User = require('../models/user');
 
-async function verifyLocationData(req, res) {
+// Check for coordinates and matching name
+async function verifyLocationData(req) {
   const { x, y } = req.body.clickCoordinates;
   const { itemName } = req.body;
 
-  // Check for coordinates and matching name
   const validLocation = await Location.findOne({
     $and: [
       { leftBorderCoordinate: { $lte: x } },
@@ -16,14 +18,36 @@ async function verifyLocationData(req, res) {
       { name: itemName },
     ],
   });
-  console.log(validLocation);
-  return res.send(validLocation ? 'true' : 'false');
+  if (validLocation) return true;
+  return false;
 }
 
-exports.verifyLocation = [
+exports.verifyLocation = asyncHandler(async (req, res) => {
   // TODO: sanitize?
 
-  asyncHandler(async (req, res) => {
-    verifyLocationData(req, res);
-  }),
-];
+  const itemValid = await verifyLocationData(req);
+  if (!itemValid) return res.send({ itemValid: false, gameFinished: false });
+
+  const { userToken, itemName } = req.body;
+  let currentUser;
+
+  try {
+    currentUser = await User.findOne({ userId: userToken }).exec();
+  } catch (error) {
+    throw new Error('Unable to find user in database');
+  }
+
+  try {
+    const foundItem = currentUser.items.find((item) => item.name === itemName);
+    foundItem.foundByUser = true;
+    currentUser.markModified('items'); // Mongoose won't detect the change without this
+    await currentUser.save();
+  } catch (error) {
+    throw new Error('Unable to update found items in database');
+  }
+
+  if (currentUser.items.every((item) => item.foundByUser))
+    return res.send({ itemValid: true, gameFinished: true });
+
+  return res.send({ itemValid: true, gameFinished: false });
+});
